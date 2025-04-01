602d8fbdd1f7f8b6820cf0e8571f5e3601974169
WANIYAM <164039644+WANIYAM@users.noreply.github.com>
2025-04-01 16:24:07 +0500

    05_loops

---
+++ Assignment_00_to_05/05_loops_control_flow/03_wholesome_machine.py
@@ -0,0 +1,18 @@
+'''
+Write a program which prompts the user to type an affirmation of your choice (we'll use "I am capable of doing anything I put my mind to.") until they type it correctly. Sometimes, especially in the midst of such uncertain times, we just need to be reminded that we are resilient, capable, and strong; this little Python program may be able to help!
+
+Here's a sample run of the program (user input is in blue):
+
+Please type the following affirmation: I am capable of doing anything I put my mind to. Hmmm That was not the affirmation. Please type the following affirmation: I am capable of doing anything I put my mind to. I am capable of doing anything I put my mind to. That's right! :)
+
+Note that you can call input() with no prompt and it will still wait for a user to type something!
+'''
+
+def main():
+    print("Delete this line and write your code here! :)")
+
+
+# This provided line is required at the end of
+# Python file to call the main() function.
+if __name__ == '__main__':
+    main()
