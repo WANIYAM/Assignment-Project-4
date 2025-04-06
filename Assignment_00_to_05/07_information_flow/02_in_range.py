3ece09de5990f420d07cf85c04cb14dd07f1f4a2
WANIYAM <164039644+WANIYAM@users.noreply.github.com>
2025-04-06 22:18:16 +0500

    07 information flow

---
+++ Assignment_00_to_05/07_information_flow/02_in_range.py
@@ -0,0 +1,22 @@
+'''
+Implement the following function which takes in 3 integers as parameters:
+
+def in_range(n, low, high) """ Returns True if n is between low and high, inclusive. high is guaranteed to be greater than low. """
+
+'''
+
+
+def in_range(n, low, high):
+    """Returns True if n is between low and high, inclusive."""
+    return low <= n <= high
+
+def main():
+   
+    n = int(input("Enter a number: "))
+    low = int(input("Enter the lower bound: "))
+    high = int(input("Enter the upper bound: "))
+    
+    print(in_range(n, low, high))
+
+if __name__ == '__main__':
+    main()
