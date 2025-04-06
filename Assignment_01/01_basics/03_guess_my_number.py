0cce4532dd014da188c60b4b12f233f2aaac9086
WANIYAM <164039644+WANIYAM@users.noreply.github.com>
2025-04-06 22:32:12 +0500

    01 basics

---
+++ Assignment_01/01_basics/03_guess_my_number.py
@@ -0,0 +1,32 @@
+'''
+Guess My Number
+
+I am thinking of a number between 0 and 99... Enter a guess: 50 Your guess is too high
+
+Enter a new number: 25 Your guess is too low
+
+Enter a new number: 40 Your guess is too low
+
+Enter a new number: 45 Your guess is too low
+
+Enter a new number: 48 Congrats! The number was: 48
+
+'''
+import random
+
+def main():
+    secret_number = random.randint(0, 99)
+
+    guess = int(input("I am thinking of a number between 0 and 99... Enter a guess: "))
+
+    while guess != secret_number:
+        if guess > secret_number:
+            print("Your guess is too high")
+        else:
+            print("Your guess is too low")
+        guess = int(input("Enter a new number: "))
+
+    print(f"Congrats! The number was: {secret_number}")
+
+if __name__ == '__main__':
+    main()
